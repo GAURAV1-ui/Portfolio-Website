e640dfdc6b78dfa0804f79039f72f85cf5c14c83
GAURAV <iamgaurav201@gmail.com>
2023-05-07 21:29:12 +0530

    resume updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import { BrowserRouter } from "react-router-dom";
+
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components"
+
 
 function App() {
-
-
   return (
     <>
       <h1> 3D Developer Portfolio</h1>
